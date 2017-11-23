6649a6f4b3d646e1ef336d170538f410af66a9c0
kthxpy <ferrerorochermfg@gmail.com>
2017-11-23 14:33:23 +0100

    Test for invalid direction created

--- test_sn.py
+++ test_sn.py
@@ -11,3 +11,13 @@ def test_isOccupied_Negative():
     x, y = 0, 1
     result = isOccupied(entities, x, y)
     assert result == False
+
+def test_move_invalid_direction():
+    entities = [(0,0), (1,0)]
+
+    error = False
+    try:
+        move(entities, "f")
+    except ValueError:
+        error = True
+    assert error == True 
