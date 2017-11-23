e106b52de0b1ddee2575738095411ab15f0c33e9
Lucie Pinterova <lucieofic@gmail.com>
2017-11-23 12:23:56 +0100

    isOccupied test created

---
+++ test_sn.py
@@ -0,0 +1,13 @@
+from sn import isOccupied
+
+def test_isOccupied_Positive():
+    entities = [(0,0), (1,0), (2,0)]
+    x, y = 1, 0
+    result = isOccupied(entities, x, y)
+    assert result == True
+
+def test_isOccupied_Negative():
+    entities = [(0,0), (1,0), (2,0)]
+    x, y = 0, 1
+    result = isOccupied(entities, x, y)
+    assert result == False
