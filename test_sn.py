860eeb7a03c0a1ba6d4abc6eeb4ffee07d251df6
kthxpy <ferrerorochermfg@gmail.com>
2017-11-26 17:11:47 +0100

    test fix

--- test_sn.py
+++ test_sn.py
@@ -1,23 +1,26 @@
 from sn import isOccupied, move
 
 def test_isOccupied_Positive():
-    entities = [(0,0), (1,0), (2,0)]
+    listOfEntities = [(0,0), (1,0), (2,0)]
     x, y = 1, 0
-    result = isOccupied(entities, x, y)
+    result = isOccupied(listOfEntities, x, y)
     assert result == True
 
 def test_isOccupied_Negative():
-    entities = [(0,0), (1,0), (2,0)]
+    listOfEntities = [(0,0), (1,0), (2,0)]
     x, y = 0, 1
-    result = isOccupied(entities, x, y)
+    result = isOccupied(listOfEntities, x, y)
     assert result == False
 
 def test_move_succes():
-    entities = [(0,0), (1,0), (2,0)]
+    entities =  {
+                    "body"  : [(0, 0), (1, 0), (2, 0)],
+                    "food"  : [(2, 1)],
+                }
     results = []
     levelData = {
-                    "width"     : 5,
-                    "height"    : 5
+                    "width"  : 5,
+                    "height" : 5
                 }
     directions = [ "j", "j", "z", "z", "s", "v"]
     states = [
@@ -30,14 +33,17 @@ def test_move_succes():
              ]
     for i in range(len(states)):
         move(entities, levelData, directions[i])
-        results.append(entities == states[i] )
+        results.append(entities["body"] == states[i] )
     assert results == [True, True, True, True, True, True]
 
 def test_move_invalid_direction():
-    entities = [(0,0), (1,0), (2,0)]
+    entities =  {
+                    "body"  : [(0, 0), (1, 0), (2, 0)],
+                    "food"  : [(2, 1)],
+                }
     levelData = {
                     "width"     : 5,
-                    "height"    : 5
+                    "height"    : 5,
                 }
     error = False
     try:
