1ff97e230ff372c2e6e29d6ce5c8e02c85693870
ktxbye <sakrauz@gmail.com>
2017-11-30 16:20:42 +0100

    Test fix

--- test_sn.py
+++ test_sn.py
@@ -1,13 +1,13 @@
 from sn import isOccupied, move, genFood
 
 def test_isOccupied_Positive():
-    listOfEntities = [(0,0), (1,0), (2,0)]
+    listOfEntities = [(0, 0), (1, 0), (2, 0)]
     x, y = 1, 0
     result = isOccupied(listOfEntities, x, y)
     assert result == True
 
 def test_isOccupied_Negative():
-    listOfEntities = [(0,0), (1,0), (2,0)]
+    listOfEntities = [(0, 0), (1, 0), (2, 0)]
     x, y = 0, 1
     result = isOccupied(listOfEntities, x, y)
     assert result == False
@@ -15,20 +15,20 @@ def test_isOccupied_Negative():
 def test_move_succes():
     results = []
     levelData = {
-                    "width"  : 5,
-                    "height" : 5,
-                    "body"   : [(0, 0), (1, 0), (2, 0)],
-                    "food"   : [],
-                }
-    directions = [ "j", "j", "z", "z", "s", "v"]
+        "width"  : 5,
+        "height" : 5,
+        "body"   : [(0, 0), (1, 0), (2, 0)],
+        "food"   : [],
+    }
+    directions = ["j", "j", "z", "z", "s", "v"]
     states = [
-                [(1,0), (2,0), (2,1)],
-                [(2,0), (2,1), (2,2)],
-                [(2,1), (2,2), (3,2)],
-                [(2,2), (3,2), (4,2)],
-                [(3,2), (4,2), (4,1)],
-                [(4,2), (4,1), (3,1)]
-             ]
+        [(1, 0), (2, 0), (2, 1)],
+        [(2, 0), (2, 1), (2, 2)],
+        [(2, 1), (2, 2), (3, 2)],
+        [(2, 2), (3, 2), (4, 2)],
+        [(3, 2), (4, 2), (4, 1)],
+        [(4, 2), (4, 1), (3, 1)]
+    ]
     for i in range(len(states)):
         move(levelData, directions[i])
         results.append(levelData["body"] == states[i] )
